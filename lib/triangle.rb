a55ccb79e0b37a02e6a29309e5bbeaaba255de7b
Karly Mareka <karly.mareka@gmail.com>
2019-02-13 03:28:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,8 @@ class Triangle
   end
   
   def kind
+    if @side1 == 0 || @side2 == 0 || @side3 == 0 
+      raise TriangleError 
     if @side1 == @side2 && @side1 == @side3
       return :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3 

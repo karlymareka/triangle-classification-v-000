1fc87d35da2e55f952858c22519d655389a2b23b
Karly Mareka <karly.mareka@gmail.com>
2019-02-13 03:26:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,5 +13,8 @@ class Triangle
       return :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3 
       return :isosceles 
+    else 
+      return :scalene
+    end 
   end 
 end
